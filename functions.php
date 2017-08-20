40e45c54e07263c1d4178017c23f3b1e2aa4f19c
Erin Black <erinblackdesign@gmail.com>
2017-08-20 14:36:34 -0500

    index.php done

--- functions.php
+++ functions.php
@@ -3,9 +3,9 @@
   //Souce Wordpress Treehouse theme styles
   function wpt_theme_styles(){
     //take you from root directory to foundations.css file
-    wp_enqueue_style( 'foundation_css', get_template_directory_uri() . 'css/foundation.css' );
+    wp_enqueue_style( 'foundation_css', get_template_directory_uri() . '/css/foundation.css' );
     //take you from root directory to normalize.css file
-    wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
+    // wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
     //google fonts url
     wp_enqueue_style( 'googlefont_css', 'url(http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic);');
     //take you from root directory to style.css
