28c267755962d5b9542e6199a01d8124b640e42e
Erin Black <erinblackdesign@gmail.com>
2017-08-20 14:21:10 -0500

    got header and footer files to populate

--- footer.php
+++ footer.php
@@ -1,1 +1,7 @@
-Footer
+<div class="footer-clear"></div>
+<footer class="row no-max pad">
+<p>Copywrite <?php echo date('Y'); ?></p>
+</footer>
+  <?php wp_footer(); ?>
+</body>
+</html>
--- functions.php
+++ functions.php
@@ -3,31 +3,21 @@
   //Souce Wordpress Treehouse theme styles
   function wpt_theme_styles(){
     //take you from root directory to foundations.css file
-    wp_enqueue_style('foundation_css', get_template_directory_uri() .
-    'css/foundations.css' );
+    wp_enqueue_style( 'foundation_css', get_template_directory_uri() . 'css/foundation.css' );
     //take you from root directory to normalize.css file
-    wp_enqueue_style('foundation_css', get_template_directory_uri() .
-    'css/normalize.css' );
+    wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
     //google fonts url
-    wp_enqueue_style('foundation_css',
-    'url(http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic);');
+    wp_enqueue_style( 'googlefont_css', 'url(http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic);');
     //take you from root directory to style.css
-    wp_enqueue_style('foundation_css', get_template_directory_uri() .
-    '/style.css' );
+    wp_enqueue_style('main_css', get_template_directory_uri() . '/style.css' );
   }// end wpt_theme_styles
   add_action('wp_enqueue_scripts', 'wpt_theme_styles' );
 
   //Source JS files to theme
   function wpt_theme_js(){
-    wp_enqueue_script('modernizr_js', get_template_directory_uri() .
-    '/js/modernizr.js', '', '', false );
-
-    wp_enqueue_script('foundation_js', get_template_directory_uri() .
-    '/js/foundation.min.js', array('jquery'), '', true );
-
-    wp_enqueue_script('app_js', get_template_directory_uri() .
-    '/js/app.js', array('jquery', 'foundation_js'), '', true );
-
+    wp_enqueue_script( 'modernizr_js', get_template_directory_uri() . '/js/modernizr.js', '', '', false );
+    wp_enqueue_script( 'foundation_js', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '', true );
+    wp_enqueue_script( 'app_js', get_template_directory_uri() . '/js/app.js', array('jquery', 'foundation_js'), '', true );
     add_action('wp_enqueue_scripts', 'wpt_theme_js' );
   } // end wpt_theme_js
  ?>
