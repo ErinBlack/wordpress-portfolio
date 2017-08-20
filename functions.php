dfe4769d4a21fc2defaa49a0325f40c3570094f6
Erin Black <erinblackdesign@gmail.com>
2017-08-20 15:18:42 -0500

    added menu to functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,19 @@
 <?php
 
+  add_theme_support('menus');
+
+  
+  //adding nav menus
+  function register_theme_menus(){
+
+    register_nav_menus(
+      array(
+        'primary-menu' => __('Primary Menu')
+      )
+    );
+  } // end register_theme_menus
+
+  add_action('init', 'register_theme_menus');
   //Souce Wordpress Treehouse theme styles
   function wpt_theme_styles(){
     //take you from root directory to foundations.css file
@@ -7,7 +21,7 @@
     //take you from root directory to normalize.css file
     // wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/css/normalize.css' );
     //google fonts url
-    wp_enqueue_style( 'googlefont_css', 'url(http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic);');
+    wp_enqueue_style( 'googlefont_css', 'http://fonts.googleapis.com/css?family=Asap:400,700,400italic,700italic' );
     //take you from root directory to style.css
     wp_enqueue_style('main_css', get_template_directory_uri() . '/style.css' );
   }// end wpt_theme_styles
