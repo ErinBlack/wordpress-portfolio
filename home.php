e03b7022a5b687540f8004ea2fab0c7c87e06b2f
Erin Black <erinblackdesign@gmail.com>
2017-08-21 11:59:53 -0500

    got posts to display with authors

--- home.php
+++ home.php
@@ -8,8 +8,26 @@
         <div class="primary">
           <!-- Have Posts  -->
           <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
-            <h1><?php the_title(); ?></h1>
-            <p><?php the_content(); ?></p>
+            <!-- Single Blog Post Information -->
+            <article class="post">
+            <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+            <h2><?php echo strip_tags( get_the_excerpt() ); ?></h2>
+              <ul class="post-meta no-bullet">
+                <li class="author">
+                    <span class="wpt-avatar small">
+                      <?php echo get_avatar( get_the_author_meta( 'ID' ), 24 ); ?>
+                    </span>
+                    by <?php the_author_posts_link(); ?>
+                </li>
+                <li class="cat">in <?php the_category(); ?></li>
+                <li class="date"> <?php the_date (); ?></li>
+              </ul>
+              <?php if(the_post_thumbnail() ) :  ?>
+              <div class="img-container">
+               <?php the_post_thumbnail('large'); ?>
+              </div>
+            <?php endif; ?>
+            </article>
             <!-- No posts  -->
           <?php endwhile; else :?>
             <p><?php _e( 'Sorry, no pages found.'); ?></p>
